b5d3d4305bdd322aa20b3bde6ada496fad533f28
Diego Molina <diemol@gmail.com>
2019-10-26 23:29:30 +0200

    Improving deploy script [deploy site]

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ if git --no-pager log -1 --oneline | grep "\\[deploy site\\]"; then
 
   if [ -n "$GITHUB_AUTH_SECRET" ]
   then
+      echo -e "\033[0;32mAdding Selenium CI Bot credentials...\033[0m"
       touch ~/.git-credentials
       chmod 0600 ~/.git-credentials
       echo $GITHUB_AUTH_SECRET > ~/.git-credentials
@@ -13,12 +14,15 @@ if git --no-pager log -1 --oneline | grep "\\[deploy site\\]"; then
       git config user.name "Selenium CI Bot"
   fi
 
-  git checkout master
+  git remote add github "https://selenium-ci:${GITHUB_AUTH_SECRET}@github.com/SeleniumHQ/site.git"
+  git fetch github
+  # git stash save || true
+  git checkout -t github/master -b github/master
   git pull
   chmod +x "$TRAVIS_BUILD_DIR"/build-site.sh && "$TRAVIS_BUILD_DIR"/build-site.sh
   git add .
   git commit -m "Deploying on $(date), commit ${TRAVIS_COMMIT} and job ${TRAVIS_JOB_NUMBER}, [skip ci]" || true
-  git push
+  git push github HEAD:master
 else
   echo -e "\033[0;32mCommit to master did not trigger deployment, add [deploy site] to the commit message if you intend to deploy...\033[0m"
 fi
