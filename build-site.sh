e2eaa57932f643d4fb77ccd1f99e7367897cf184
Diego Molina <diemol@users.noreply.github.com>
2020-08-16 10:45:09 +0200

    Update build-site.sh

--- build-site.sh
+++ build-site.sh
@@ -6,17 +6,17 @@ if [[ -z "${DEPLOY_PRIME_URL}" ]]; then
 else
   echo -e "\033[0;32mNetlify DEPLOY_PRIME_URL detected, this seems to be a PR, deployment happening at ${DEPLOY_PRIME_URL}...\033[0m"
   USE_BASE_URL_SITE="-b ${DEPLOY_PRIME_URL}"
-  USE_BASE_URL_SITE_DOCS="-b ${DEPLOY_PRIME_URL}/documentation"
+  USE_BASE_URL_DOCS="-b ${DEPLOY_PRIME_URL}/documentation"
 fi
 
 echo -e "\033[0;32mDeleting Hugo previously generated directories for docs and main site...\033[0m"
 rm -rf site_source_files/public && rm -rf docs_source_files/public
 
 echo -e "\033[0;32mGenerating Hugo site for docs...\033[0m"
-cd docs_source_files && hugo --minify && cd ..
+cd docs_source_files && hugo --minify ${URL_BASE_DOCS} && cd ..
 
 echo -e "\033[0;32mGenerating Hugo site for main website...\033[0m"
-cd site_source_files && hugo --minify && cd ..
+cd site_source_files && hugo --minify ${URL_BASE_SITE} && cd ..
 
 echo -e "\033[0;32mMerging both sites into a single one...\033[0m"
 mv docs_source_files/public/* site_source_files/public/documentation
