e975d51d7901dc8182ba0bd1520a289f83b2ef1e
Diego Molina <diemol@users.noreply.github.com>
2020-08-17 10:39:33 +0200

    Making Netlify previews work properly (#500)

--- build-site.sh
+++ build-site.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
+set -x
 
 if [[ -z "${DEPLOY_PRIME_URL}" ]]; then
-  USE_BASE_URL_SITE=""  
-  USE_BASE_URL_SITE_DOCS=""
+  USE_BASE_URL_SITE=""
+  USE_BASE_URL_DOCS=""
 else
   echo -e "\033[0;32mNetlify DEPLOY_PRIME_URL detected, this seems to be a PR, deployment happening at ${DEPLOY_PRIME_URL}...\033[0m"
-  USE_BASE_URL_SITE="-b ${DEPLOY_PRIME_URL}"
-  USE_BASE_URL_DOCS="-b ${DEPLOY_PRIME_URL}/documentation"
+  USE_BASE_URL_SITE="--baseURL ${DEPLOY_PRIME_URL}"
+  USE_BASE_URL_DOCS="--baseURL ${DEPLOY_PRIME_URL}/documentation"
 fi
 
 echo -e "\033[0;32mDeleting Hugo previously generated directories for docs and main site...\033[0m"
 rm -rf site_source_files/public && rm -rf docs_source_files/public
 
 echo -e "\033[0;32mGenerating Hugo site for docs...\033[0m"
-cd docs_source_files && hugo --minify ${URL_BASE_DOCS} && cd ..
+cd docs_source_files && hugo --minify ${USE_BASE_URL_DOCS} && cd ..
 
 echo -e "\033[0;32mGenerating Hugo site for main website...\033[0m"
-cd site_source_files && hugo --minify ${URL_BASE_SITE} && cd ..
+cd site_source_files && hugo --minify ${USE_BASE_URL_SITE} && cd ..
 
 echo -e "\033[0;32mMerging both sites into a single one...\033[0m"
 mv docs_source_files/public/* site_source_files/public/documentation
