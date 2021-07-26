626b1bdcdfad47f8f0d8af6a8384039b08de7164
Diego Molina <diemol@gmail.com>
2021-07-26 11:47:58 +0200

    Updating scripts instead of netlify file

--- build-site.sh
+++ build-site.sh
@@ -6,23 +6,26 @@ set -e
 
 if [[ -z "${DEPLOY_PRIME_URL}" ]]; then
   USE_BASE_URL_SITE=""
-  USE_BASE_URL_DOCS=""
 else
   echo -e "\033[0;32mNetlify DEPLOY_PRIME_URL detected, this seems to be a PR, deployment happening at ${DEPLOY_PRIME_URL}...\033[0m"
   USE_BASE_URL_SITE="--baseURL ${DEPLOY_PRIME_URL}"
-  USE_BASE_URL_DOCS="--baseURL ${DEPLOY_PRIME_URL}/documentation"
 fi
 
-echo -e "\033[0;32mDeleting Hugo previously generated directories for docs and main site...\033[0m"
-rm -rf site_source_files/public && rm -rf docs_source_files/public
+"cd website_hugo_files/themes/docsy && git submodule update -f --init && cd ../.. && hugo"
 
-echo -e "\033[0;32mGenerating Hugo site for docs...\033[0m"
-cd docs_source_files && hugo --minify ${USE_BASE_URL_DOCS} && cd ..
+echo -e "\033[0;32mDeleting Hugo previously generated directories...\033[0m"
+rm -rf website_hugo_files/public
 
-echo -e "\033[0;32mGenerating Hugo site for main website...\033[0m"
-cd site_source_files && hugo --minify ${USE_BASE_URL_SITE} && cd ..
+echo -e "\033[0;32mSwitching to Docsy theme directory...\033[0m"
+cd website_hugo_files/themes/docsy
 
-echo -e "\033[0;32mMerging both sites into a single one...\033[0m"
-mv docs_source_files/public/* site_source_files/public/documentation
+echo -e "\033[0;32mGit init for Docsy...\033[0m"
+git submodule update -f --init
+
+echo -e "\033[0;32mGoing back to site root directory...\033[0m"
+cd ../..
+
+echo -e "\033[0;32mGenerating Hugo site for website...\033[0m"
+hugo --minify ${USE_BASE_URL_SITE}
 
 echo -e "\033[0;32mDone building site!\033[0m"
