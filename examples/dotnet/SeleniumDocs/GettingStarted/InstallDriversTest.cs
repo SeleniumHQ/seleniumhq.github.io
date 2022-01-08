29265709663e388a6ed638bcce73f91fab7dca18
titusfortner <titusfortner@gmail.com>
2022-01-08 10:02:39 -0600

    dotnet getting started code examples

---
+++ examples/dotnet/SeleniumDocs/GettingStarted/InstallDriversTest.cs
@@ -0,0 +1,57 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Safari;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SeleniumDocs
+{
+    [TestClass]
+    public class InstallDriversTest
+    {
+        [TestMethod]
+        public void ChromeSession()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+
+            var driver = new ChromeDriver();
+
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void EdgeSession()
+        {
+            new DriverManager().SetUpDriver(new EdgeConfig());
+
+            var driver = new EdgeDriver();
+
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void FirefoxSession()
+        {
+            new DriverManager().SetUpDriver(new FirefoxConfig());
+
+            var driver = new FirefoxDriver();
+
+            driver.Quit();
+        }
+
+        [Ignore] // Only runs on Windows
+        [TestMethod]
+        public void InternetExplorerSession()
+        {
+            new DriverManager().SetUpDriver(new InternetExplorerConfig());
+
+            var driver = new InternetExplorerDriver();
+
+            driver.Quit();
+        }
+    }
+}
