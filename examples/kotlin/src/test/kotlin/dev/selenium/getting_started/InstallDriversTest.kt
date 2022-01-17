022f9db548ce97476ca45675579c693005601846
Krishnan Mahadevan <krishnan.mahadevan1978@gmail.com>
2022-01-17 17:32:33 +0530

    Adding Kotlin sample project (#925)
    Co-authored-by: Diego Molina <diemol@users.noreply.github.com>

---
+++ examples/kotlin/src/test/kotlin/dev/selenium/getting_started/InstallDriversTest.kt
@@ -0,0 +1,42 @@
+package dev.selenium.getting_started
+
+import io.github.bonigarcia.wdm.WebDriverManager
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.edge.EdgeDriver
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.ie.InternetExplorerDriver
+
+
+class InstallDriversTest {
+    @Test
+    fun chromeSession() {
+        WebDriverManager.chromedriver().setup()
+        val driver: WebDriver = ChromeDriver()
+        driver.quit()
+    }
+
+    @Test
+    fun edgeSession() {
+        WebDriverManager.edgedriver().setup()
+        val driver: WebDriver = EdgeDriver()
+        driver.quit()
+    }
+
+    @Test
+    fun firefoxSession() {
+        WebDriverManager.firefoxdriver().setup()
+        val driver: WebDriver = FirefoxDriver()
+        driver.quit()
+    }
+
+    @Disabled("Only runs on Windows")
+    @Test
+    fun ieSession() {
+        WebDriverManager.iedriver().setup()
+        val driver: WebDriver = InternetExplorerDriver()
+        driver.quit()
+    }
+}
