962dbf15b12358285d416adec9747af32a217228
Diego Molina <diemol@users.noreply.github.com>
2022-08-18 22:11:01 +0200

    Update build-site.sh

--- build-site.sh
+++ build-site.sh
@@ -15,7 +15,7 @@ if [[ -z "${DEPLOY_PRIME_URL}" ]]; then
 else
   echo -e "\033[0;32mNetlify DEPLOY_PRIME_URL detected, this seems to be a PR, deployment happening at ${DEPLOY_PRIME_URL}...\033[0m"
   USE_BASE_URL_SITE="--baseURL ${DEPLOY_PRIME_URL}"
-  if git rev-parse --quiet --verify ${SELENIUM_EXAMPLES_BRANCH} > /dev/null; then
+  if git rev-parse --quiet --verify ${HEAD} > /dev/null; then
     SELENIUM_EXAMPLES_BRANCH=${HEAD}
   else 
     SELENIUM_EXAMPLES_BRANCH=trunk
