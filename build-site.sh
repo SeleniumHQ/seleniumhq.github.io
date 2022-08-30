fa61ac6e2fd828693a6a7d378898b36eac7fdf34
Diego Molina <diemol@gmail.com>
2022-08-30 16:20:15 +0200

    Build site working for Netlify now

--- build-site.sh
+++ build-site.sh
@@ -15,12 +15,14 @@ fi
 
 USE_BASE_URL_SITE=""
 if [[ "${NETLIFY}" = "true" ]]; then
-  echo -e "\033[0;32mNetlify detected, seems to be a PR, deployment happening at ${DEPLOY_PRIME_URL}...\033[0m"
+  echo -e "\033[0;32mNetlify detected, deployment happening at ${DEPLOY_PRIME_URL}...\033[0m"
   USE_BASE_URL_SITE="--baseURL ${DEPLOY_PRIME_URL}"
   if [[ "${PULL_REQUEST}" = "true" ]]; then
+    echo -e "\033[0;32mDeploying a preview for a pull request in Netlify...\033[0m"
     # Getting repo information from the GitHub API
     # Useful to get the code examples from the right org and repo
     REPO_INFO=$(curl -s -H ${SELENIUM_ACCEPT_HEADER} -H ${SELENIUM_AUTH_HEADER} ${SELENIUM_GITHUB_API_PULLS_URL}/${REVIEW_ID})
+    SELENIUM_EXAMPLES_BRANCH=${HEAD}
     SELENIUM_EXAMPLES_REPO=$(echo $REPO_INFO | jq -r .head.repo.name)
     SELENIUM_EXAMPLES_ORG=$(echo $REPO_INFO | jq -r .head.repo.owner.login)
   fi
