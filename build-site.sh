63e8706fcda7bcd7cde494bb445c3bf580e6c404
Diego Molina <diemol@users.noreply.github.com>
2022-08-30 13:27:25 +0200

    Update build-site.sh

--- build-site.sh
+++ build-site.sh
@@ -17,14 +17,15 @@ if [[ -z "${DEPLOY_PRIME_URL}" ]]; then
 else
   echo -e "\033[0;32mNetlify DEPLOY_PRIME_URL detected, this seems to be a PR, deployment happening at ${DEPLOY_PRIME_URL}...\033[0m"
   USE_BASE_URL_SITE="--baseURL ${DEPLOY_PRIME_URL}"
+  git ls-remote --heads origin ${HEAD}
   if git ls-remote --heads origin ${HEAD} > /dev/null; then    
-    SELENIUM_EXAMPLES_BRANCH=${HEAD}
-    echo -e "\033[0;32mTrusted branch found, SELENIUM_EXAMPLES_BRANCH ${SELENIUM_EXAMPLES_BRANCH}...\033[0m"
-  else 
     echo -e "\033[0;32mPull request comes from a fork. Using trunk branch to render code examples pulled from GitHub.\033[0m"
     echo -e "\033[0;32mCreate a new branch in the upstream repo, merge this PR into that branch, and create a new PR from a protected branch.\033[0m"
     SELENIUM_EXAMPLES_BRANCH=trunk
     echo -e "\033[0;Using trunk... SELENIUM_EXAMPLES_BRANCH ${SELENIUM_EXAMPLES_BRANCH}...\033[0m"
+  else 
+    SELENIUM_EXAMPLES_BRANCH=${HEAD}
+    echo -e "\033[0;32mTrusted branch found, SELENIUM_EXAMPLES_BRANCH ${SELENIUM_EXAMPLES_BRANCH}...\033[0m"
   fi
 fi
 
