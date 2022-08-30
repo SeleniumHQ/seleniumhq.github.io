3635f44ab7dac0cf1d59289079a704f9471dc028
Diego Molina <diemol@gmail.com>
2022-08-30 15:55:29 +0200

    Getting env vars from GH API for Netlify build

--- build-site.sh
+++ build-site.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
-# Print commands
-set -x
 # Exit on error
 set -e
 
-SELENIUM_EXAMPLES_BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+# curl -s -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${SELENIUM_CI_TOKEN}" \
+#   https://api.github.com/repos/SeleniumHQ/seleniumhq.github.io/pulls/1145 | jq .head.repo
 
-if [[ "${CI}" = "true" ]]; then
+SELENIUM_GITHUB_API_PULLS_URL=https://api.github.com/repos/SeleniumHQ/seleniumhq.github.io/pulls
+SELENIUM_ACCEPT_HEADER="Accept: application/vnd.github+json"
+SELENIUM_AUTH_HEADER="Authorization: Bearer ${SELENIUM_CI_TOKEN}"
+SELENIUM_EXAMPLES_BRANCH=trunk
+SELENIUM_EXAMPLES_REPO=seleniumhq.github.io
+SELENIUM_EXAMPLES_ORG=SeleniumHQ
+
+if [[ "${GITHUB_ACTIONS}" = "true" ]]; then
   SELENIUM_EXAMPLES_BRANCH=${GITHUB_HEAD_REF}
 fi
 
-if [[ -z "${DEPLOY_PRIME_URL}" ]]; then
-  USE_BASE_URL_SITE=""
-else
-  echo -e "\033[0;32mNetlify DEPLOY_PRIME_URL detected, this seems to be a PR, deployment happening at ${DEPLOY_PRIME_URL}...\033[0m"
+USE_BASE_URL_SITE=""
+if [[ "${NETLIFY}" = "true" ]]; then
+  echo -e "\033[0;32mNetlify detected, seems to be a PR, deployment happening at ${DEPLOY_PRIME_URL}...\033[0m"
   USE_BASE_URL_SITE="--baseURL ${DEPLOY_PRIME_URL}"
-  if git rev-parse --quiet --verify ${HEAD} > /dev/null; then    
-    SELENIUM_EXAMPLES_BRANCH=${HEAD}
-  else 
-    echo -e "\033[0;32mPull request comes from a fork. Using trunk branch to render code examples pulled from GitHub.\033[0m"
-    echo -e "\033[0;32mCreate a new branch in the upstream repo, merge this PR into that branch, and create a new PR from a protected branch.\033[0m"
-    SELENIUM_EXAMPLES_BRANCH=trunk
-  fi
+  # Getting repo information from the GitHub API
+  # Useful to get the code examples from the right org and repo
+  REPO_INFO=$(curl -s -H ${SELENIUM_ACCEPT_HEADER} -H ${SELENIUM_AUTH_HEADER} ${SELENIUM_GITHUB_API_PULLS_URL}/${REVIEW_ID})
+  SELENIUM_EXAMPLES_REPO=$(echo $REPO_INFO | jq -r .head.repo.name)
+  SELENIUM_EXAMPLES_ORG=$(echo $REPO_INFO | jq -r .head.repo.owner.login)
 fi
 
 echo -e "\033[0;32mDeleting Hugo previously generated directories...\033[0m"
@@ -34,6 +37,7 @@ echo -e "\033[0;32mSwitching to Docsy theme directory...\033[0m"
 cd website_and_docs && npm install
 
 echo -e "\033[0;32mGenerating Hugo site for website...\033[0m"
-SELENIUM_EXAMPLES_BRANCH=${SELENIUM_EXAMPLES_BRANCH} hugo --minify ${USE_BASE_URL_SITE}
+echo -e "\033[0;32mUsing SELENIUM_EXAMPLES_REPO=${SELENIUM_EXAMPLES_REPO} SELENIUM_EXAMPLES_ORG=${SELENIUM_EXAMPLES_ORG} SELENIUM_EXAMPLES_BRANCH=${SELENIUM_EXAMPLES_BRANCH} hugo --minify ${USE_BASE_URL_SITE}...\033[0m"
+SELENIUM_EXAMPLES_REPO=${SELENIUM_EXAMPLES_REPO} SELENIUM_EXAMPLES_ORG=${SELENIUM_EXAMPLES_ORG} SELENIUM_EXAMPLES_BRANCH=${SELENIUM_EXAMPLES_BRANCH} hugo --minify ${USE_BASE_URL_SITE}
 
 echo -e "\033[0;32mDone building site!\033[0m"
