e103be1e01e9697ec6625613c12ffb1ec77572a8
titusfortner <titusfortner@gmail.com>
2022-09-04 22:16:47 -0500

    implement setting persistent language from URL query
    [deploy site]

--- website_and_docs/static/js/tabpane-persist.js
+++ website_and_docs/static/js/tabpane-persist.js
@@ -1,5 +1,15 @@
+<!-- Upstream docsy commit — ecd4be87ea48e8e94684e32c925049e9bdf7f127-->
 if (typeof Storage !== 'undefined') {
-    const activeLanguage = localStorage.getItem('active_language');
+    let activeLanguage = localStorage.getItem('active_language');
+
+    // Get active language from URL params if exists
+    const params = new Proxy(new URLSearchParams(window.location.search), {
+        get: (searchParams, prop) => searchParams.get(prop),
+    });
+    if (params.language !== null) {
+        activeLanguage = params.language;
+        localStorage.setItem('active_language', activeLanguage);
+    }
     if (activeLanguage) {
         document
             .querySelectorAll('.persistLang-' + activeLanguage)
