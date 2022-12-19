c974d7ef056c6dc767bf82dff8600ece9d8b5c77
Sriharsha <sri_harsha509@hotmail.com>
2022-12-19 18:03:52 +0530

    Add code sample for chrome command line args

---
+++ examples/javascript/test/browser/chromeSpecificCaps.spec.js
@@ -0,0 +1,17 @@
+const Chrome = require('selenium-webdriver/chrome');
+const {suite} = require('selenium-webdriver/testing');
+const options = new Chrome.Options();
+
+suite(function (env) {
+  describe('Should be able to Test Command line arguments', function () {
+    it('headless', async function () {
+      let driver = await env
+        .builder()
+        .setChromeOptions(options.addArguments('headless'))
+        .build();
+
+      await driver.get('https://www.google.com');
+      await driver.quit();
+    });
+  });
+});
