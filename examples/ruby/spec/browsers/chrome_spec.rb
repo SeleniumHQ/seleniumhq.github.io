33cc7a71c7303090f77538212f715508b4a0d613
Sriharsha <sri_harsha509@hotmail.com>
2023-01-07 21:09:30 +0530

    [rb] Add ruby code sample for chrome args

--- examples/ruby/spec/browsers/chrome_spec.rb
+++ examples/ruby/spec/browsers/chrome_spec.rb
@@ -3,8 +3,16 @@
 require 'spec_helper'
 
 RSpec.describe 'Chrome' do
+  let(:driver) { start_session }
+
   it 'basic options' do
     options = Selenium::WebDriver::Options.chrome
     @driver = Selenium::WebDriver.for :chrome, options: options
+  end
+
+  it 'add arguments' do
+    options = Selenium::WebDriver::Options.chrome(args: ['--headless=chrome'])
+    driver = Selenium::WebDriver.for :chrome, options: options
+    driver.get('https://www.google.com')
   end
 end
