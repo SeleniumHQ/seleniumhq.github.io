9670a4c373f50b7763a5eec3d8a32a7cb299c8fb
Omer Jonuzi <omerjonuzi7@gmail.com>
2023-01-18 14:05:28 +0100

    Update example section for keeping browser open

--- examples/python/tests/browsers/test_chrome.py
+++ examples/python/tests/browsers/test_chrome.py
@@ -7,3 +7,9 @@ def test_basic_options():
     driver = webdriver.Chrome(options=options)
 
     driver.quit()
+
+def test_keep_browser_open():
+    chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True)
+    
+    driver = webdriver.Chrome(options=chrome_options)
