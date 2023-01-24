603367ec5fbbf89fa58b2a9bf69b4b69a9178f8b
Sriharsha <sri_harsha509@hotmail.com>
2023-01-24 19:47:24 +0530

    [py] Add chrome specific options

--- examples/python/tests/browsers/test_chrome.py
+++ examples/python/tests/browsers/test_chrome.py
@@ -7,3 +7,12 @@ def test_basic_options():
     driver = webdriver.Chrome(options=options)
 
     driver.quit()
+
+def test_keep_browser_open():
+    chrome_options = ChromeOptions()
+    chrome_options.add_experimental_option("detach", True)
+
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get('http://selenium.dev')
+
+    driver.quit()
