1f3a66f71086452f94f6086c7a2530f42ac447dd
Sriharsha <sri_harsha509@hotmail.com>
2023-01-30 18:52:52 +0530

    [JS] Move safari code to examples

---
+++ examples/javascript/test/browser/safariSpecificCap.spec.js
@@ -0,0 +1,15 @@
+const safari = require('selenium-webdriver/safari');
+const {Builder} = require("selenium-webdriver");
+const options = new safari.Options();
+
+describe('Should be able to Test Command line arguments', function () {
+  it('headless', async function () {
+    let driver = new Builder()
+      .forBrowser('safari')
+      .setSafariOptions(options)
+      .build();
+
+    await driver.get('https://www.google.com');
+    await driver.quit();
+  });
+});
