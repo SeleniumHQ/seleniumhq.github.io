63077bc99905ded57d5a939d034c2cc13e697651
Zach Attas <zach.attas@gmail.com>
2023-03-28 16:03:24 -0500

    updates to 4.8.5 and removes logging

--- examples/ruby/spec/browsers/internet_explorer_spec.rb
+++ examples/ruby/spec/browsers/internet_explorer_spec.rb
@@ -4,11 +4,7 @@ require 'spec_helper'
 
 RSpec.describe 'Internet Explorer', exclusive: {platform: :windows} do
   it 'basic options' do
-    Selenium::WebDriver.logger.level = :debug
     options = Selenium::WebDriver::Options.ie
     @driver = Selenium::WebDriver.for :ie, options: options
-  end
-  after(:each) do 
-    Selenium::WebDriver.logger.level = :error
   end
 end
