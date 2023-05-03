d9614084c94daf34eee457bd83028bb4a4944c3a
jyotipatne <132436478+jyotipatne@users.noreply.github.com>
2023-05-03 13:04:43 +0530

    Update InstallDriversTest.java

--- examples/java/src/test/java/dev/selenium/getting_started/InstallDriversTest.java
+++ examples/java/src/test/java/dev/selenium/getting_started/InstallDriversTest.java
@@ -10,44 +10,29 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class InstallDriversTest {
-    @Test
-    @Disabled("Do not run in CI")
-    public void chromeSession() {
-        WebDriverManager.chromedriver().setup();
-
-        WebDriver driver = new ChromeDriver();
-
-        driver.quit();
-    }
-
-    @Test
-    @Disabled("Do not run in CI")
-    public void edgeSession() {
-        WebDriverManager.edgedriver().setup();
-
-        WebDriver driver = new EdgeDriver();
-
-        driver.quit();
-    }
-
-    @Test
-    @Disabled("Do not run in CI")
-    public void firefoxSession() {
-        WebDriverManager.firefoxdriver().setup();
-
-        WebDriver driver = new FirefoxDriver();
-
-        driver.quit();
-    }
-
-    @Test
-    @Disabled("Do not run in CI")
-    public void ieSession() {
-        WebDriverManager.iedriver().setup();
-
-        WebDriver driver = new InternetExplorerDriver();
-
-        driver.quit();
-    }
-}
+public class Third {  
+  
+    public static void main(String[] args) {  
+      
+           // System Property for Chrome Driver   
+        System.setProperty("webdriver.chrome.driver", "D:\\ChromeDriver\\chromedriver.exe");  
+          
+             // Instantiate a ChromeDriver class.     
+        WebDriver driver=new ChromeDriver();  
+          
+           // Launch Website  
+        driver.navigate().to("http://www.javatpoint.com/");  
+          
+         //Maximize the browser  
+          driver.manage().window().maximize();  
+          
+          //Scroll down the webpage by 5000 pixels  
+        JavascriptExecutor js = (JavascriptExecutor)driver;  
+        js.executeScript("scrollBy(0, 5000)");   
+          
+         // Click on the Search button  
+        driver.findElement(By.linkText("Core Java")).click();     
+  
+    }  
+  
+}  
