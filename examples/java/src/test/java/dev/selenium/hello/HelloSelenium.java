62ddc188b97f524a11f10bf3975a5b4425250877
AMAN KUMAR ASBARIYA <88770592+AmanAsbariya@users.noreply.github.com>
2023-06-15 10:05:45 +0530

    Update HelloSelenium.java
    Added some missing code. Please merge

--- examples/java/src/test/java/dev/selenium/hello/HelloSelenium.java
+++ examples/java/src/test/java/dev/selenium/hello/HelloSelenium.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HelloSelenium {
     public static void main(String[] args) {
+        System.setProperty("key","driver path");
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://selenium.dev");
