02297e8a76192b923742c2599c391ebb209140b9
Sriharsha <sriharsha_sureshbabu@epam.com>
2023-06-26 19:46:22 +0530

    [js] Code sample for installAddon and uninstallAddon for firefox

--- examples/javascript/test/browser/firefoxSpecificFunctionalities.js
+++ examples/javascript/test/browser/firefoxSpecificFunctionalities.js
@@ -1,8 +1,9 @@
-const {Browser} = require('selenium-webdriver');
+const {Browser, By} = require('selenium-webdriver');
 const Firefox = require('selenium-webdriver/firefox');
 const options = new Firefox.Options();
 const path = require('path');
 const {suite} = require("selenium-webdriver/testing");
+const assert = require("assert");
 
 
 suite(function (env) {
@@ -19,11 +20,14 @@ describe('Should be able to Test Command line arguments', function () {
   it('Should be able to add extension', async function () {
 
     const xpiPath = path.resolve('./test/resources/extensions/selenium-example.xpi')
-    let driver = await env.builder()
-      .setFirefoxOptions(options.addExtensions(xpiPath))
-      .build();
+    let driver = await env.builder().build();
+    let id = await driver.installAddon(xpiPath);
+    await driver.uninstallAddon(id);
+
 
     await driver.get('https://www.selenium.dev/selenium/web/blank.html');
+    const ele = await driver.findElements(By.id("webextensions-selenium-example"));
+    assert.equal(ele.length, 0);
     await driver.quit();
   });
 });
