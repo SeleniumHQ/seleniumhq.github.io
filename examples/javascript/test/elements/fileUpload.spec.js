02c80df483f50c8c1dac2252a67b933706aeae19
Sriharsha <sriharsha_sureshbabu@epam.com>
2023-06-29 20:57:08 +0530

    [JS] Enable test for file uplaod

--- examples/javascript/test/elements/fileUpload.spec.js
+++ examples/javascript/test/elements/fileUpload.spec.js
@@ -1,5 +1,6 @@
 const { suite } = require('selenium-webdriver/testing');
 const {Browser, By} = require("selenium-webdriver");
+const path = require("path");
 
 suite(function(env) {
   describe('File Upload Test', function() {
@@ -11,14 +12,16 @@ suite(function(env) {
 
     after(() => driver.quit());
 
-    xit('Should be able to upload a file successfully', async function() {
+    it('Should be able to upload a file successfully', async function() {
+      const image = path.resolve('./test/resources/selenium-snapshot.png')
+
       await driver.manage().setTimeouts({implicit: 5000});
 
       // Navigate to URL
       await driver.get('https://www.selenium.dev/selenium/web/upload.html');
 
       // Upload snapshot
-      await driver.findElement(By.id("upload")).sendKeys("selenium-snapshot.jpg");
+      await driver.findElement(By.id("upload")).sendKeys(image);
 
       await driver.findElement(By.id("go")).submit();
     });
