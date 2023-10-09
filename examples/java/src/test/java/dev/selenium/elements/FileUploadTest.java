3f7ce5437c341cdf0a674c167c8617a8e253a8e2
Sri Harsha <12621691+harsha509@users.noreply.github.com>
2023-10-08 21:27:03 -0400

    Fix indentation [deploy site]

--- examples/java/src/test/java/dev/selenium/elements/FileUploadTest.java
+++ examples/java/src/test/java/dev/selenium/elements/FileUploadTest.java
@@ -10,27 +10,26 @@ import java.time.Duration;
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.Duration;
 
 public class FileUploadTest {
 
     @Test
     public void fileUploadTest() {
     WebDriver driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(1000));
-		driver.get("https://the-internet.herokuapp.com/upload");
-		Path path = Paths.get("src/test/resources/selenium-snapshot.png");
+    driver.manage().timeouts().implicitlyWait(Duration.ofMillis(1000));
+    driver.get("https://the-internet.herokuapp.com/upload");
+    Path path = Paths.get("src/test/resources/selenium-snapshot.png");
     File imagePath = new File(path.toUri());
 
-		//we want to import selenium-snapshot file.
-		driver.findElement(By.id("file-upload")).sendKeys(imagePath.toString());
-		driver.findElement(By.id("file-submit")).submit();
-		if(driver.getPageSource().contains("File Uploaded!")) {
-			System.out.println("file uploaded");
-		}
-		else{
-				System.out.println("file not uploaded");
-			}
-		driver.quit();
+    //we want to import selenium-snapshot file.
+    driver.findElement(By.id("file-upload")).sendKeys(imagePath.toString());
+    driver.findElement(By.id("file-submit")).submit();
+   if(driver.getPageSource().contains("File Uploaded!")) {
+	System.out.println("file uploaded");
+   }
+   else{
+        System.out.println("file not uploaded");
+       }
+    driver.quit();
     }
 }
