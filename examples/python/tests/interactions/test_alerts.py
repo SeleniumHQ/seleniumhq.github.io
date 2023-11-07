ab5c3222381ccae2dbc09c5859ea50ceced9e0e1
zjt-code <77527193+zjt-code@users.noreply.github.com>
2023-11-07 22:34:32 +0800

    Update test_alerts.py
    add example code

--- examples/python/tests/interactions/test_alerts.py
+++ examples/python/tests/interactions/test_alerts.py
@@ -1,2 +1,20 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
+
+driver = webdriver.Chrome()
+driver.get('https://www.selenium.dev/zh-cn/documentation/webdriver/interactions/alerts/')
+print(driver.page_source)
+
+alert_dialog = driver.find_element(by=By.LINK_TEXT,value=""See an example alert"")
+alert_dialog.click()
+# Wait for the alert to be displayed and store it in a variable
+alert = WebDriverWait(driver,10).until(EC.alert_is_present())
+
+print(alert.text)
+# Press the OK button
+alert.accept()
+
+driver.quit()
