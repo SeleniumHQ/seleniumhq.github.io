1e4861a941757a0eec56f544a598d755b2d6df53
Puja Jagani <puja.jagani93@gmail.com>
2024-02-16 18:24:48 +0530

    [bidi][java] Add script commands code examples

--- examples/java/src/test/java/dev/selenium/bidirectional/webdriver_bidi/ScriptTest.java
+++ examples/java/src/test/java/dev/selenium/bidirectional/webdriver_bidi/ScriptTest.java
@@ -3,20 +3,29 @@ package dev.selenium.bidirectional.webdriver_bidi;
 import dev.selenium.BaseTest;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.bidi.LogInspector;
 import org.openqa.selenium.bidi.Script;
 import org.openqa.selenium.bidi.browsingcontext.BrowsingContext;
 import org.openqa.selenium.bidi.browsingcontext.ReadinessState;
+import org.openqa.selenium.bidi.log.ConsoleLogEntry;
+import org.openqa.selenium.bidi.script.ChannelValue;
 import org.openqa.selenium.bidi.script.EvaluateResult;
 import org.openqa.selenium.bidi.script.EvaluateResultExceptionValue;
 import org.openqa.selenium.bidi.script.EvaluateResultSuccess;
 import org.openqa.selenium.bidi.script.LocalValue;
 import org.openqa.selenium.bidi.script.RealmInfo;
+import org.openqa.selenium.bidi.script.RealmType;
 import org.openqa.selenium.bidi.script.RemoteReference;
 import org.openqa.selenium.bidi.script.ResultOwnership;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -193,7 +202,7 @@ class ScriptTest extends BaseTest {
     }
 
     @Test
-    void  canEvaluateInARealm() {
+    void canEvaluateInARealm() {
         String tab = driver.getWindowHandle();
         try (Script script = new Script(tab, driver)) {
             List<RealmInfo> realms = script.getAllRealms();
@@ -208,7 +217,7 @@ class ScriptTest extends BaseTest {
     }
 
     @Test
-    void  canDisownHandle() {
+    void canDisownHandle() {
         String window = driver.getWindowHandle();
         try (Script script = new Script(window, driver)) {
             BrowsingContext context = new BrowsingContext(driver, window);
@@ -219,9 +228,10 @@ class ScriptTest extends BaseTest {
 
             getLocatedElement(driver, By.id("box0"));
 
-            String boxId = (String) ((JavascriptExecutor)driver).executeScript(
-                    "var box = document.querySelector('.redbox');" +
-                            "return box.id;");
+            EvaluateResult result =
+                    script.evaluateFunctionInBrowsingContext(
+                            window, "document.querySelector('.redbox');", false, Optional.of(ResultOwnership.ROOT));
+            String boxId = ((EvaluateResultSuccess)result).getResult().getHandle().get();
 
             script.disownBrowsingContextScript(
                             window, List.of(boxId));
@@ -231,12 +241,168 @@ class ScriptTest extends BaseTest {
                             RemoteReference.Type.HANDLE, boxId);
 
             // Since the handle is now eligible for garbage collections, it is no longer available to be used.
-            Assertions.assertThrows(WebDriverException.class, () ->script.callFunctionInBrowsingContext(
+            Assertions.assertThrows(WebDriverException.class, () -> script.callFunctionInBrowsingContext(
                     window,
                     "arg => arg.a",
                     false, Optional.of(List.of(value)),
                     Optional.empty(),
                     Optional.empty()));
+        }
+    }
+
+    @Test
+    void canDisownHandleInARealm() {
+        String window = driver.getWindowHandle();
+        try (Script script = new Script(window, driver)) {
+            BrowsingContext context = new BrowsingContext(driver, window);
+
+            context.navigate("https://www.selenium.dev/selenium/web/dynamic.html", ReadinessState.COMPLETE);
+
+            driver.findElement(By.id("adder")).click();
+
+            getLocatedElement(driver, By.id("box0"));
+
+            List<RealmInfo> realms = script.getAllRealms();
+            String realmId = realms.get(0).getRealmId();
+
+            // Retrieve the handle for the element added to DOM
+            EvaluateResult result =
+                    script.evaluateFunctionInBrowsingContext(
+                            window, "document.querySelector('.redbox');", false, Optional.of(ResultOwnership.ROOT));
+            String boxId = ((EvaluateResultSuccess)result).getResult().getHandle().get();
+
+            LocalValue value =
+                    LocalValue.remoteReference(
+                            RemoteReference.Type.HANDLE, boxId);
+
+            EvaluateResult checkHandle = script.callFunctionInBrowsingContext(
+                    window,
+                    "arg => arg.a",
+                    false, Optional.of(List.of(value)),
+                    Optional.empty(),
+                    Optional.empty());
+
+            // The handle is present in memory, else it would result in exception
+            Assertions.assertEquals(EvaluateResult.Type.SUCCESS, checkHandle.getResultType());
+
+            // Useful to memory management in a dynamic webpage where DOM mutations happen often
+            script.disownRealmScript(realmId, List.of(boxId));
+
+            // Since the handle is now eligible for garbage collections, it is no longer available to be used.
+            Assertions.assertThrows(WebDriverException.class, () -> script.callFunctionInBrowsingContext(
+                    window,
+                    "arg => arg.a",
+                    false, Optional.of(List.of(value)),
+                    Optional.empty(),
+                    Optional.empty()));
+        }
+    }
+
+    @Test
+    void canGetAllRealms() {
+        String firstWindow = driver.getWindowHandle();
+        String secondWindow = driver.switchTo().newWindow(WindowType.WINDOW).getWindowHandle();
+        try (Script script = new Script(firstWindow, driver)) {
+            List<RealmInfo> realms = script.getAllRealms();
+            Assertions.assertEquals(2, realms.size());
+        }
+    }
+
+    @Test
+    void canGetRealmByType() {
+        String firstWindow = driver.getWindowHandle();
+        String secondWindow = driver.switchTo().newWindow(WindowType.WINDOW).getWindowHandle();
+        try (Script script = new Script(firstWindow, driver)) {
+            List<RealmInfo> realms = script.getRealmsByType(RealmType.WINDOW);
+            Assertions.assertEquals(2, realms.size());
+        }
+    }
+
+    @Test
+    void canGetRealmInBrowsingContext() {
+        String windowId = driver.getWindowHandle();
+        String tabId = driver.switchTo().newWindow(WindowType.TAB).getWindowHandle();
+        try (Script script = new Script(windowId, driver)) {
+            List<RealmInfo> realms = script.getRealmsInBrowsingContext(tabId);
+            Assertions.assertEquals(1, realms.size());
+        }
+    }
+
+    @Test
+    void canGetRealmInBrowsingContextByType() {
+        String windowId = driver.getWindowHandle();
+        String tabId = driver.switchTo().newWindow(WindowType.TAB).getWindowHandle();
+        try (Script script = new Script(windowId, driver)) {
+            List<RealmInfo> windowRealms =
+                    script.getRealmsInBrowsingContextByType(windowId, RealmType.WINDOW);
+            Assertions.assertEquals(1, windowRealms.size());
+        }
+    }
+
+    @Test
+    void canAddPreloadScript() throws ExecutionException, InterruptedException, TimeoutException {
+        try (Script script = new Script(driver)) {
+            String id =
+                    script.addPreloadScript("() => {{ console.log('{preload_script_console_text}') }}");
+
+            Assertions.assertNotNull(id);
+
+            try (LogInspector logInspector = new LogInspector(driver)) {
+                CompletableFuture<ConsoleLogEntry> future = new CompletableFuture<>();
+                logInspector.onConsoleEntry(future::complete);
+
+                driver.get("https://www.selenium.dev/selenium/blankPage");
+
+                ConsoleLogEntry logEntry = future.get(5, TimeUnit.SECONDS);
+
+                Assertions.assertEquals("{preload_script_console_text}", logEntry.getText());
+            }
+        }
+    }
+
+    @Test
+    void canAddPreloadScriptWithArguments() {
+        try (Script script = new Script(driver)) {
+            String id =
+                    script.addPreloadScript(
+                            "(channel) => channel('will_be_send', 'will_be_ignored')",
+                            List.of(new ChannelValue("channel_name")));
+            Assertions.assertNotNull(id);
+        }
+    }
+
+    @Test
+    void canAddPreloadScriptInASandbox() {
+        try (Script script = new Script(driver)) {
+            String id = script.addPreloadScript("() => { window.bar=2; }", "sandbox");
+            Assertions.assertNotNull(id);
+            driver.get("https://www.selenium.dev/selenium/blankPage");
+
+            EvaluateResult result =
+                    script.evaluateFunctionInBrowsingContext(
+                            driver.getWindowHandle(), "sandbox", "window.bar", true, Optional.empty());
+            Assertions.assertEquals(EvaluateResult.Type.SUCCESS, result.getResultType());
+        }
+    }
+
+    @Test
+    void canRemovePreloadScript() throws ExecutionException, InterruptedException, TimeoutException {
+        try (Script script = new Script(driver)) {
+            String id =
+                    script.addPreloadScript("() => {{ console.log('{preload_script_console_text}') }}");
+
+            Assertions.assertNotNull(id);
+
+            try (LogInspector logInspector = new LogInspector(driver)) {
+                CompletableFuture<ConsoleLogEntry> future = new CompletableFuture<>();
+                logInspector.onConsoleEntry(future::complete);
+
+                script.removePreloadScript(id);
+
+                driver.get("https://www.selenium.dev/selenium/blankPage");
+
+                Assertions.assertThrows(TimeoutException.class, () -> future.get(5, TimeUnit.SECONDS));
+            }
         }
     }
 }
