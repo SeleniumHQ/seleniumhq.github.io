dad00d5720c84494ab78552cb25109688ac3d059
Puja Jagani <puja.jagani93@gmail.com>
2024-02-16 15:56:00 +0530

    [java] Fix imports in code examples

--- examples/java/src/test/java/dev/selenium/bidirectional/webdriver_bidi/ScriptTest.java
+++ examples/java/src/test/java/dev/selenium/bidirectional/webdriver_bidi/ScriptTest.java
@@ -7,7 +7,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.bidi.Script;
-import org.openqa.selenium.bidi.script.*;
+import org.openqa.selenium.bidi.script.EvaluateResult;
+import org.openqa.selenium.bidi.script.EvaluateResultExceptionValue;
+import org.openqa.selenium.bidi.script.EvaluateResultSuccess;
+import org.openqa.selenium.bidi.script.RealmInfo;
+import org.openqa.selenium.bidi.script.ResultOwnership;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
@@ -183,6 +187,21 @@ class ScriptTest extends BaseTest {
 
     @Test
     void  canEvaluateInARealm() {
+        String tab = driver.getWindowHandle();
+        try (Script script = new Script(tab, driver)) {
+            List<RealmInfo> realms = script.getAllRealms();
+            String realmId = realms.get(0).getRealmId();
+
+            EvaluateResult result =
+                    script.evaluateFunctionInRealm(
+                            realmId, "window.foo", true, Optional.empty());
+
+            Assertions.assertEquals(EvaluateResult.Type.SUCCESS, result.getResultType());
+        }
+    }
+
+    @Test
+    void  canDisownHandles() {
         String tab = driver.getWindowHandle();
         try (Script script = new Script(tab, driver)) {
             List<RealmInfo> realms = script.getAllRealms();
