02078d83f343677bd09cb09ee20e66a4389aa582
Alpha <AlphaWong@users.noreply.github.com>
2024-03-06 18:47:56 +0000

    doc: update the 101 page

--- examples/javascript/test/hello/helloSelenium.js
+++ examples/javascript/test/hello/helloSelenium.js
@@ -1,7 +1,7 @@
-const {Builder} = require('selenium-webdriver');
+const {By, Builder, Browser} = require('selenium-webdriver');
 
 (async function helloSelenium() {
-  let driver = await new Builder().forBrowser('chrome').build();
+  let driver = await new Builder().forBrowser(Browser.CHROME).build();
 
   await driver.get('https://selenium.dev');
 
