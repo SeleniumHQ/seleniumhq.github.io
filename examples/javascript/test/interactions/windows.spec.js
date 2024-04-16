ed9a6702c0f13c4fe4009afe13faf155060f2414
Sri Harsha <sri_harsha@epam.com>
2024-04-16 21:58:11 +0530

    [JS] Add code sample for printPage(w3c)

---
+++ examples/javascript/test/interactions/windows.spec.js
@@ -0,0 +1,30 @@
+const {Builder} = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const assert = require("node:assert");
+let opts = new chrome.Options();
+opts.addArguments('--headless');
+
+let startIndex = 0
+let endIndex = 5
+let pdfMagicNumber = 'JVBER'
+let base64Code
+
+describe('Interactions - Windows', function () {
+  let driver;
+
+  before(async function () {
+    driver = await new Builder().forBrowser('chrome').setChromeOptions(opts).build();
+  });
+
+  after(async () => await driver.quit());
+
+  it('Should be able to print page to pdf', async function () {
+    await driver.get('https://www.selenium.dev/selenium/web/alerts.html');
+    let base64 = await driver.printPage({pageRanges: ["1-2"]});
+    // page can be saved as PDF as below
+    // await fs.writeFileSync('./test.pdf', base64, 'base64');
+
+    base64Code = base64.slice(startIndex, endIndex)
+    assert.strictEqual(base64Code, pdfMagicNumber)
+  });
+});
