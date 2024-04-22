90dd06ecd28b37387de1849c2881d64838baab63
Diego Molina <diemol@gmail.com>
2024-04-22 15:44:47 +0200

    Removing quotes from jq response

--- scripts/latest-nightly-version.ps1
+++ scripts/latest-nightly-version.ps1
@@ -5,5 +5,5 @@ $PATH_PACKAGES_API = "orgs/seleniumhq/packages/$PACKAGE_TYPE/$PACKAGE_NAME/versi
 $ACCEPT_HEADER = "Accept: application/vnd.github+json"
 $VERSION_HEADER = "X-GitHub-Api-Version: 2022-11-28"
 
-$ghApiCommand = "gh api -H `"$ACCEPT_HEADER`" -H `"$VERSION_HEADER`" $PATH_PACKAGES_API | jq '.[0].name'"
+$ghApiCommand = "gh api -H `"$ACCEPT_HEADER`" -H `"$VERSION_HEADER`" $PATH_PACKAGES_API | jq -r '.[0].name'"
 Invoke-Expression -Command $ghApiCommand
--- scripts/latest-nightly-version.sh
+++ scripts/latest-nightly-version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -xe
+set -e
 
 PACKAGE_TYPE="$1"
 PACKAGE_NAME="$2"
@@ -9,4 +9,4 @@ PATH_PACKAGES_API="orgs/seleniumhq/packages/$PACKAGE_TYPE/$PACKAGE_NAME/versions
 ACCEPT_HEADER="Accept: application/vnd.github+json"
 VERSION_HEADER="X-GitHub-Api-Version: 2022-11-28"
 
-gh api -H "$ACCEPT_HEADER" -H "$VERSION_HEADER" $PATH_PACKAGES_API | jq '.[0].name'
+gh api -H "$ACCEPT_HEADER" -H "$VERSION_HEADER" $PATH_PACKAGES_API | jq -r '.[0].name'
