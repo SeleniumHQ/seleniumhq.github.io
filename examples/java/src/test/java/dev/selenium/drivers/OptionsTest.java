c817f765e8daa2eb9998fcd6318e5c96df55b03e
innazh <38092381+innazh@users.noreply.github.com>
2024-08-16 21:05:02 +0000

    add browser options test examples in java
    added java examples/tests for browserName, browserVersion,platformName,
    scriptTimeout, pageLoadTimeout, implicitWaitTimeout,
    unhandledPromptBehaviour, setWindowRect, strictFileInteractability

--- examples/java/src/test/java/dev/selenium/drivers/OptionsTest.java
+++ examples/java/src/test/java/dev/selenium/drivers/OptionsTest.java
@@ -60,5 +60,95 @@ public class OptionsTest extends BaseTest {
       driver.quit();
     }
   }
+
+  	@Test
+	public void getBrowserName() {
+		ChromeOptions chromeOptions = new ChromeOptions();
+		String name = chromeOptions.getBrowserName();
+		assert !name.isEmpty();
+	}
+
+	@Test
+	public void setBrowserVersion() {
+		ChromeOptions chromeOptions = new ChromeOptions();
+		String expected = "latest";
+		chromeOptions.setBrowserVersion(expected);
+		assert expected.equals(chromeOptions.getBrowserVersion());
+	}
+
+	@Test
+	public void setPlatformName() {
+		ChromeOptions chromeOptions = new ChromeOptions();
+		String expected = "OS X 10.6";
+		chromeOptions.setPlatformName(expected);
+		Platform name = chromeOptions.getPlatformName();
+		assert name.toString().equals(expected);
+	}
+
+	@Test
+	public void setScriptTimeout() {
+		ChromeOptions chromeOptions = new ChromeOptions();
+		Duration expected = Duration.of(5, ChronoUnit.SECONDS);
+		chromeOptions.setScriptTimeout(expected);
+
+		WebDriver driver = new ChromeDriver(chromeOptions);
+		try {
+			Duration timeout = driver.manage().timeouts().getScriptTimeout();
+			assert timeout.equals(expected);
+		} finally {
+			driver.quit();
+		}
+	}
+
+	@Test
+	public void setPageLoadTimeout() {
+		ChromeOptions chromeOptions = new ChromeOptions();
+		Duration expected = Duration.of(5, ChronoUnit.SECONDS);
+		chromeOptions.setPageLoadTimeout(expected);
+
+		WebDriver driver = new ChromeDriver(chromeOptions);
+		try {
+			Duration timeout = driver.manage().timeouts().getPageLoadTimeout();
+			assert timeout.equals(expected);
+		} finally {
+			driver.quit();
+		}
+	}
+
+	@Test
+	public void setImplicitWaitTimeout() {
+		ChromeOptions chromeOptions = new ChromeOptions();
+		Duration expected = Duration.of(5, ChronoUnit.SECONDS);
+		chromeOptions.setImplicitWaitTimeout(expected);
+
+		WebDriver driver = new ChromeDriver(chromeOptions);
+		try {
+			Duration timeout = driver.manage().timeouts().getImplicitWaitTimeout();
+			assert timeout.equals(expected);
+		} finally {
+			driver.quit();
+		}
+	}
+
+	@Test
+	public void setUnhandledPromptBehaviour() {
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.setUnhandledPromptBehaviour(UnexpectedAlertBehaviour.DISMISS_AND_NOTIFY);
+		assert chromeOptions.getCapability(CapabilityType.UNHANDLED_PROMPT_BEHAVIOUR).equals(UnexpectedAlertBehaviour.DISMISS_AND_NOTIFY);
+	}
+
+	@Test
+  	public void setWindowRect() {
+    	ChromeOptions chromeOptions = new ChromeOptions();
+    	chromeOptions.setCapability(CapabilityType.SET_WINDOW_RECT, true);
+    	assert chromeOptions.getCapability(CapabilityType.SET_WINDOW_RECT).equals(true);
+  	}
+	
+	@Test
+	public void setStrictFileInteractability() {
+	    ChromeOptions chromeOptions = new ChromeOptions();
+	    chromeOptions.setCapability(CapabilityType.STRICT_FILE_INTERACTABILITY, true);
+	    assert chromeOptions.getCapability(CapabilityType.STRICT_FILE_INTERACTABILITY).equals(true);
+	}
 }
 
