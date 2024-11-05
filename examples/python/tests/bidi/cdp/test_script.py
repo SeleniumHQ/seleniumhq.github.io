3b1f808584b270e03acf15ebf0e92c8e6d073f05
Viet Nguyen Duc <nguyenducviet4496@gmail.com>
2024-11-05 13:07:17 +0700

    Fix flaky test in examples python
    Signed-off-by: Viet Nguyen Duc <nguyenducviet4496@gmail.com>

--- examples/python/tests/bidi/cdp/test_script.py
+++ examples/python/tests/bidi/cdp/test_script.py
@@ -1,6 +1,8 @@
 import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.log import Log
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 @pytest.mark.trio
@@ -8,6 +10,7 @@ async def test_mutation(driver):
     async with driver.bidi_connection() as session:
         async with Log(driver, session).mutation_events() as event:
             driver.get('https://www.selenium.dev/selenium/web/dynamic.html')
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "reveal")))
             driver.find_element(By.ID, "reveal").click()
 
     assert event["element"] == driver.find_element(By.ID, "revealed")
