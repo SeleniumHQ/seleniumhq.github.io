946fb97a1039605e7cce469bbefe2618e3cbff85
delta456 <swstkbaranwal@gmail.com>
2024-11-14 00:13:20 +0530

    remove test attr

--- examples/java/src/test/java/dev/selenium/browsers/SafariTest.java
+++ examples/java/src/test/java/dev/selenium/browsers/SafariTest.java
@@ -33,8 +33,7 @@ public class SafariTest {
 
         driver = new SafariDriver(service);
     }
-
-    @Test
+    
     public void safariTechnologyPreview() {
         SafariOptions options = new SafariOptions();
         options.setUseTechnologyPreview(true);
