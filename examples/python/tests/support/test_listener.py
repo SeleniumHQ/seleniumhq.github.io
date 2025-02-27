b622defe0b8d2f7b4f9194e4fac3359e3485138e
touero <touer0018@gmail.com>
2025-02-27 11:57:48 +0800

    fix(py): Add some test for listeners

--- examples/python/tests/support/test_listener.py
+++ examples/python/tests/support/test_listener.py
@@ -1,2 +1,25 @@
+import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.events import EventFiringWebDriver, AbstractEventListener
 
+TEST_URL = "https://www.selenium.dev/selenium/web/clicks.html"
+
+class MyListener(AbstractEventListener):
+    def before_click(self, element, driver):
+        print(f"Before click: {element.text}")
+        assert element.text == "I'm a normal link"
+
+    def after_navigate_to(self, url, driver):
+        print(f"After navigating to: {url}")
+        assert url == TEST_URL
+
+
+def test_listener():
+    driver = webdriver.Chrome()
+    event_driver = EventFiringWebDriver(driver, MyListener())
+    event_driver.get(TEST_URL)
+    time.sleep(2)
+    element = event_driver.find_element(By.ID, "normal")
+    element.click()
+    event_driver.quit()
