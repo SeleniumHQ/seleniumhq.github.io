9edbb635950ba9b9d3325e5801f3643eb41b19f4
Corey Goldberg <1113081+cgoldberg@users.noreply.github.com>
2025-04-23 22:16:26 -0400

    Remove import and adjust line numbers

--- examples/python/tests/support/test_expected_conditions.py
+++ examples/python/tests/support/test_expected_conditions.py
@@ -1,5 +1,3 @@
-import pytest
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
