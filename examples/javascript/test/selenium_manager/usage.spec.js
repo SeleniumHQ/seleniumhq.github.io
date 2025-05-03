7c332f2c06de82737f02f1fe77b216ebc7c5399e
Simon Benzer <69980130+shbenzer@users.noreply.github.com>
2025-05-03 09:13:52 -0400

    updates description, removes old test from CI

--- examples/javascript/test/selenium_manager/usage.spec.js
+++ examples/javascript/test/selenium_manager/usage.spec.js
@@ -3,7 +3,7 @@ const {Browser, Builder} = require("selenium-webdriver");
 const options = new Chrome.Options();
 
 describe('Usage Test', function () {
-  it('After Selenium Manager', async function () {
+  it('Creates driver wit Selenium Manager', async function () {
 
     let driver = new Builder()
         .forBrowser(Browser.CHROME)
@@ -13,20 +13,24 @@ describe('Usage Test', function () {
     await driver.quit();
   });
 
-  it('Before Selenium Manager', async function () {
-    let driverPath = paths.driverPath;
-    let browserPath = paths.browserPath;
+  // it('Creates driver with Selenium Manager', async function () {
+  //   const paths = {
+  //       driverPath: '/path/to/chromedriver',
+  //       browserPath: '/path/to/chrome'
+  //     };
+  //   let driverPath = paths.driverPath;
+  //   let browserPath = paths.browserPath;
 
-    options.setChromeBinaryPath(browserPath)
+  //   options.setChromeBinaryPath(browserPath)
 
-    let service = new Chrome.ServiceBuilder().setPath(driverPath)
+  //   let service = new Chrome.ServiceBuilder().setPath(driverPath)
 
-    let driver = new Builder()
-        .forBrowser(Browser.CHROME)
-        .setChromeService(service)
-        .build();
+  //   let driver = new Builder()
+  //       .forBrowser(Browser.CHROME)
+  //       .setChromeService(service)
+  //       .build();
 
-    await driver.get('https://www.selenium.dev/selenium/web/blank.html');
-    await driver.quit();
-  });
+  //   await driver.get('https://www.selenium.dev/selenium/web/blank.html');
+  //   await driver.quit();
+  // });
 });
