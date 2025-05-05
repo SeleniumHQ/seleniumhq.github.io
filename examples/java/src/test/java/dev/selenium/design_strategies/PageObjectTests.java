f1aff332ffe3f33f9b4f548e414e75502e16fe31
Simon Benzer <69980130+shbenzer@users.noreply.github.com>
2025-05-05 12:39:29 -0400

    Update PageObjectTests.java

--- examples/java/src/test/java/dev/selenium/design_strategies/PageObjectTests.java
+++ examples/java/src/test/java/dev/selenium/design_strategies/PageObjectTests.java
@@ -1,30 +1,30 @@
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 
-public class FooTest {
-  private EditIssue editIssue;
+// public class FooTest {
+//   private EditIssue editIssue;
 
-  // Removed @Before
-  public void prepareComponents() {
-    WebDriver driver = new FirefoxDriver();
+//   @Before
+//   public void prepareComponents() {
+//     WebDriver driver = new FirefoxDriver();
 
-    ProjectPage project = new ProjectPage(driver, "selenium");
-    SecuredPage securedPage = new SecuredPage(driver, project, "example", "top secret");
-    editIssue = new EditIssue(driver, securedPage);
-  }
+//     ProjectPage project = new ProjectPage(driver, "selenium");
+//     SecuredPage securedPage = new SecuredPage(driver, project, "example", "top secret");
+//     editIssue = new EditIssue(driver, securedPage);
+//   }
 
-  // Removed @Test
-  public void demonstrateNestedLoadableComponents() {
-    editIssue.get();
+//   @Test
+//   public void demonstrateNestedLoadableComponents() {
+//     editIssue.get();
 
-    editIssue.title.sendKeys('Title');
-    editIssue.body.sendKeys('What Happened');
-    editIssue.setHowToReproduce('How to Reproduce');
-    editIssue.setLogOutput('Log Output');
-    editIssue.setOperatingSystem('Operating System');
-    editIssue.setSeleniumVersion('Selenium Version');
-    editIssue.setBrowserVersion('Browser Version');
-    editIssue.setDriverVersion('Driver Version');
-    editIssue.setUsingGrid('I Am Using Grid');
-  }
+//     editIssue.title.sendKeys('Title');
+//     editIssue.body.sendKeys('What Happened');
+//     editIssue.setHowToReproduce('How to Reproduce');
+//     editIssue.setLogOutput('Log Output');
+//     editIssue.setOperatingSystem('Operating System');
+//     editIssue.setSeleniumVersion('Selenium Version');
+//     editIssue.setBrowserVersion('Browser Version');
+//     editIssue.setDriverVersion('Driver Version');
+//     editIssue.setUsingGrid('I Am Using Grid');
+//   }
 
-}
+// }
