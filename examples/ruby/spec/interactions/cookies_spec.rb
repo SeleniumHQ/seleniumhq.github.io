3119131e287c45c570d6d52be5d03f0d28c7f296
Pallavi <info@5elementslearning.dev>
2025-05-06 20:14:26 +0530

    fixed lint concerns ruby file

--- examples/ruby/spec/interactions/cookies_spec.rb
+++ examples/ruby/spec/interactions/cookies_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 RSpec.describe 'Cookies' do
   let(:driver) { start_session }
-  
+
   it 'adds a cookie' do
     driver.navigate.to 'https://www.selenium.dev/selenium/web/blank.html'
     # Add cookie into current browser context
@@ -13,7 +13,7 @@ RSpec.describe 'Cookies' do
     # Verify cookie was added
     expect(driver.manage.cookie_named('key')[:value]).to eq('value')
   end
-  
+
   it 'gets a named cookie' do
     driver.navigate.to 'https://www.selenium.dev/selenium/web/blank.html'
     # Add cookie into current browser context
@@ -23,20 +23,20 @@ RSpec.describe 'Cookies' do
     cookie = driver.manage.cookie_named('foo')
     expect(cookie[:value]).to eq('bar')
   end
-  
+
   it 'gets all cookies' do
     driver.navigate.to 'https://www.selenium.dev/selenium/web/blank.html'
     # Add cookies into current browser context
     driver.manage.add_cookie(name: 'test1', value: 'cookie1')
     driver.manage.add_cookie(name: 'test2', value: 'cookie2')
-    
+
     # Get cookies
     cookies = driver.manage.all_cookies
-    
+
     # Verify both cookies exist with correct values
     test1_cookie = cookies.find { |c| c[:name] == 'test1' }
     test2_cookie = cookies.find { |c| c[:name] == 'test2' }
-    
+
     expect(test1_cookie[:value]).to eq('cookie1')
     expect(test2_cookie[:value]).to eq('cookie2')
   end
@@ -47,7 +47,6 @@ RSpec.describe 'Cookies' do
     
     # Verify cookie exists
     expect(driver.manage.cookie_named('test1')).not_to be_nil
-    
     # Delete cookie named
     driver.manage.delete_cookie('test1')
     
