7968ee3d02c88108ccf4c80974b708b7bdcc8c41
Diego Molina <diemol@gmail.com>
2025-05-27 18:38:51 +0200

    Bumping versions

--- examples/java/src/test/java/dev/selenium/bidi/cdp/CdpApiTest.java
+++ examples/java/src/test/java/dev/selenium/bidi/cdp/CdpApiTest.java
@@ -16,9 +16,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.devtools.DevTools;
 import org.openqa.selenium.devtools.HasDevTools;
-import org.openqa.selenium.devtools.v136.browser.Browser;
-import org.openqa.selenium.devtools.v136.network.Network;
-import org.openqa.selenium.devtools.v136.network.model.Headers;
+import org.openqa.selenium.devtools.v137.browser.Browser;
+import org.openqa.selenium.devtools.v137.network.Network;
+import org.openqa.selenium.devtools.v137.network.model.Headers;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CdpApiTest extends BaseTest {
