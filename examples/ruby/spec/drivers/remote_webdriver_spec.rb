4c496875a85a4e6b17fb8aec36dca5b5313d7efb
Diego Molina <diemol@gmail.com>
2025-05-27 16:05:08 +0200

    Using already downloaded jar and waiting for uploaded files to show up

--- examples/ruby/spec/drivers/remote_webdriver_spec.rb
+++ examples/ruby/spec/drivers/remote_webdriver_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe 'Remote WebDriver' do
   let(:target_directory) { File.join(Dir.tmpdir, SecureRandom.uuid) }
   let(:wait) { Selenium::WebDriver::Wait.new(timeout: 2) }
   let(:server) do
-    Selenium::Server.get(:latest,
-                         background: true,
-                         args: %w[--selenium-manager true --enable-managed-downloads true])
+    Selenium::Server.new(File.expand_path(File.join('..', '..', '..', 'selenium-server-4.33.0.jar'), __dir__),
+                        background: true,
+                        args: %w[--selenium-manager true --enable-managed-downloads true])
   end
   let(:grid_url) { server.webdriver_url }
 
@@ -35,8 +35,8 @@ RSpec.describe 'Remote WebDriver' do
     file_input.send_keys(upload_file)
     driver.find_element(id: 'file-submit').click
 
-    file_name = driver.find_element(id: 'uploaded-files')
-    expect(file_name.text).to eq 'selenium-snapshot.png'
+    wait.until { driver.find_element(id: 'uploaded-files') }
+    expect(driver.find_element(id: 'uploaded-files').text).to eq 'selenium-snapshot.png'
   end
 
   it 'downloads' do
