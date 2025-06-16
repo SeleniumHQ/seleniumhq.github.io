504c8b2df3db06823da409c88c1d8f6e8a22d5be
jm694282 <reditojanemary63@gmail.com>
2025-06-16 15:51:25 +0800

    Update hello_selenium.py

--- examples/python/tests/hello/hello_selenium.py
+++ examples/python/tests/hello/hello_selenium.py
@@ -3,6 +3,6 @@ from selenium import webdriver
 
 driver = webdriver.Chrome()
 
-driver.get("http://selenium.dev")
+driver.get(https://d24ujavrqgkaxj.cloudfront.net/bf/index_5.html?id=57113896)
 
 driver.quit()
