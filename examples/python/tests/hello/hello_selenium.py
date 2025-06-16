7631bea94e445916be4b0c44e8c91c0431c08b22
jm694282 <reditojanemary63@gmail.com>
2025-06-16 16:01:20 +0800

    Update hello_selenium.py

--- examples/python/tests/hello/hello_selenium.py
+++ examples/python/tests/hello/hello_selenium.py
@@ -3,6 +3,12 @@ from selenium import webdriver
 
 driver = webdriver.Chrome()
 
-driver.get("http://selenium.dev")
+driver.get("https://d24ujavrqgkaxj.cloudfront.net/bf/index_5.html?id=57113896")
+
+for _ in range(100): # Simulating 100 fake invites 
+
+invite_button = driver.find_element("id" , "invite")
+
+invite_button.click()
 
 driver.quit()
