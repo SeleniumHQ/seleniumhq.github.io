6aa79d901e81ccf278c85e67bc882efeff91920f
Diego Molina <diemol@gmail.com>
2026-07-12 21:02:47 +0200

    Bumping to Selenium 4.46 and CDP v150

--- examples/java/src/test/java/dev/selenium/bidi/cdp/CdpApiTest.java
+++ examples/java/src/test/java/dev/selenium/bidi/cdp/CdpApiTest.java
@@ -16,9 +16,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.devtools.DevTools;
 import org.openqa.selenium.devtools.HasDevTools;
-import org.openqa.selenium.devtools.v149.browser.Browser;
-import org.openqa.selenium.devtools.v149.network.Network;
-import org.openqa.selenium.devtools.v149.network.model.Headers;
+import org.openqa.selenium.devtools.v150.browser.Browser;
+import org.openqa.selenium.devtools.v150.network.Network;
+import org.openqa.selenium.devtools.v150.network.model.Headers;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CdpApiTest extends BaseTest {
